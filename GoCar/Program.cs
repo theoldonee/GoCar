a9732443191c670221152a390f403ff38dc547f1
pakmmi <paulkamani9@gmail.com>
2025-03-06 21:53:28 +0400

    feat: add user input functionality

--- GoCar/Program.cs
+++ GoCar/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GoCar;
+
+Console.WriteLine("Would you like to enter a path?\nEnter 1 for yes and 2 for no. ");
+string choice = Console.ReadLine();
+
+if(choice == "2")
+{
+    FileManager.LoadFile(true);
+}
+else
+{
+    Console.WriteLine("Enter path: ");
+    FileManager.alternatePath = Console.ReadLine();
+    FileManager.LoadFile(false);
+
+}
