7c3f5de744ac3804e5d80943657d6b03d11bc97e
pakmmi <rouvaf123@gmail.com>
2025-03-07 18:07:02 +0400

    chore: created rental class

---
+++ GoCar/Rental.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoCar
+{
+    internal class Rental
+    {
+        int RentalId { get; set; }
+        string CollectionDate { get; set; }
+        string ReturnDate { get; set; }
+
+        //Car relationship
+        string CarPlateNumber { get; set; }
+        public Car Car { get; set; }
+
+        //Client relationship
+        int ClientId { get; set; }
+        Client Client { get; set; }
+    }
+}
