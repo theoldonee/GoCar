1595fff6b9098ac149bb19e84f5e17a72f916957
sarahgoburdhun <sarahgoburdhun2002@gmail.com>
2025-03-08 19:37:09 +0400

    Update UnitTest1.cs

--- GoCar.xUnitTest/UnitTest1.cs
+++ GoCar.xUnitTest/UnitTest1.cs
@@ -2,12 +2,12 @@ namespace GoCar.xUnitTest
 {
     public class CarRentalTest()
     {
-
+        private readonly UserRepository _userRepo
         [Fact]
         public void RegisterUser_ShouldBeValid()
         {
             var user = new User {email = "zook@hotmail.com", password = "zook123"};
-            Assert.True(result);
+            TBD
         }
     }
 }
