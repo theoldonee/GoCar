4eba4ce3bf5d7799c31d0a4fcc6d049d36ded636
theoldonee <rouvaf123@gmail.com>
2025-03-09 04:17:50 +0400

    feat: add database check
    check if database contains vales

--- GoCar/Program.cs
+++ GoCar/Program.cs
@@ -17,6 +17,8 @@ string gocarAscii = @"
 Console.WriteLine(gocarAscii);
 
 // Check if there is data present in the car database
+bool databaseCheckResult = CheckCarDb();
+
 // IF yes, ask user if they would like to load database  or use file
 // IF no, Ask user if they would like to use default file or enter their own file
 
@@ -26,3 +28,27 @@ Console.WriteLine(gocarAscii);
 // Remove uses a unique ID
 // Add requires all information
 // Search can be don by the attributes of the class
+
+
+bool CheckCarDb()
+{
+    Console.WriteLine("Checking Database...");
+    bool databaseNotEmpty = false;
+    using (var contex = new CarRentalContex())
+    {
+        var cars = contex.Car.ToList();
+
+        if (cars.Count > 0)
+        {
+            databaseNotEmpty = true;
+        }
+    }
+
+    if (databaseNotEmpty)
+    {
+        return true;
+    }
+    return false;
+}
+
+
