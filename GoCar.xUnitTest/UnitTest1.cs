44c699942fb021034a974262bfd23173511f0536
theoldonee <rouvaf123@gmail.com>
2025-03-09 22:56:15 +0400

    fix: add end of statement marker, close methods and initialize car object with correct attribute and attribute types.

--- GoCar.xUnitTest/UnitTest1.cs
+++ GoCar.xUnitTest/UnitTest1.cs
@@ -3,21 +3,22 @@ namespace GoCar.xUnitTest
     public class CarRentalTest
     {
         // Declare field to store an instance of CarRepository 
-        private readonly CarRepository _carrEPO
+        private readonly CarRepository _carrEPO;
 
         public CarRentalTest()
         {
-        // create temp db for testing
+            // create temp db for testing
             var options = new DbContextOptionsBuilder<ProductContext>().UseInMemoryDatabase(databaseName: "test_database").Options;
             var dbContext = new DatabaseContext(options);
             _carRepo = new CarRepository(dbContext);
-            
+        }
+
         [Fact]
         public void AddCaar_IncreasesAmountofCars()
         {
             // Create new car object
-            var car = new Car { Model = "Zookmobile", Type = "Sport's Car", Year = "2021", Availability = "true" };
-            
+            var car = new Car { CarId="Z345-ZM89", Model = "Zookmobile", Type = "Sport's Car", Year = 2021, Available = true };
+
             // AddCar method to save car in db
             _carRepo.AddCar(car);
 
