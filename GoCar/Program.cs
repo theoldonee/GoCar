3278c9ae5cad51f24c08b65d598d52b009967b0c
theoldonee <rouvaf123@gmail.com>
2025-03-09 04:10:36 +0400

    chore: add go car Ascii art

--- GoCar/Program.cs
+++ GoCar/Program.cs
@@ -1,16 +1,19 @@
 ﻿using GoCar;
+using Microsoft.EntityFrameworkCore;
 
-Console.WriteLine("Would you like to enter a path?\nEnter 1 for yes and 2 for no. ");
-string choice = Console.ReadLine();
+string gocarAscii = @"
 
-if(choice == "2")
-{
-    FileManager.LoadFile(true);
-}
-else
-{
-    Console.WriteLine("Enter path: ");
-    FileManager.alternatePath = Console.ReadLine();
-    FileManager.LoadFile(false);
+         ░▒▓██████▓▒░ ░▒▓██████▓▒░        ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░  
+        ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+        ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+        ░▒▓█▓▒▒▓███▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓████████▓▒░▒▓███████▓▒░  
+        ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+        ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+         ░▒▓██████▓▒░ ░▒▓██████▓▒░        ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  
 
-}
+
+
+";
+Console.WriteLine(gocarAscii);
+
+
