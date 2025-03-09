fde9b8304c4b034fda845a3336c0419720d91220
theoldonee <rouvaf123@gmail.com>
2025-03-09 04:13:15 +0400

    chore: add comments defining program structure

--- GoCar/Program.cs
+++ GoCar/Program.cs
@@ -16,4 +16,13 @@ string gocarAscii = @"
 ";
 Console.WriteLine(gocarAscii);
 
+// Check if there is data present in the car database
+// IF yes, ask user if they would like to load database  or use file
+// IF no, Ask user if they would like to use default file or enter their own file
 
+// DATA IN DATA STRUCT
+// User can select an operation, selects a table, enter information
+
+// Remove uses a unique ID
+// Add requires all information
+// Search can be don by the attributes of the class
