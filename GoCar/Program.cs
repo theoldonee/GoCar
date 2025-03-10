22cb3c6479de789154843ce0bbfa7d0b0ec59982
theoldonee <rouvaf123@gmail.com>
2025-03-10 23:37:38 +0400

    refactor : add AddCar function.

--- GoCar/Program.cs
+++ GoCar/Program.cs
@@ -1,4 +1,5 @@
-﻿using GoCar;
+﻿using System.Globalization;
+using GoCar;
 using Microsoft.EntityFrameworkCore;
 
 string gocarAscii = @"
@@ -40,6 +41,7 @@ List<Car> CarList;
 List<Client> ClientList;
 List<Rental> RentalList;
 // User can select an operation, selects a table, enter information
+AddToDatabase();
 
 // Remove uses a unique ID
 // Add requires all information
@@ -74,57 +76,26 @@ void LoadDatabase()
     //var rentalStorage;
 }
 
-void AddToDatabase()
+bool AddToDatabase()
 {
     //put added object into desired table
-    Console.WriteLine("Choose a table to add to\n1: Car\n2: Client\n3: Rental");
+    Console.WriteLine("Choose a table to add to\n1: Car\n2: Client\n3: Rental\n");
     Console.WriteLine("Please enter a number:");
     string choice = Console.ReadLine();
+
     if (choice == "1")
     {
-        Console.WriteLine("Car Id:");
-        string carId = Console.ReadLine();
-        // validate car Id
-        // Use validator class
-
-        Console.WriteLine("Make:");
-        string make = Console.ReadLine();
-
-        Console.WriteLine("Model:");
-        string model = Console.ReadLine();
-
-        Console.WriteLine("Fuel type:");
-        string fuelType = Console.ReadLine();
-        // validate fuel type
-
-        Console.WriteLine("Type:");
-        string type = Console.ReadLine();
-
-        Console.WriteLine("Year:");
-        int year = Int32.Parse(Console.ReadLine());
-
-        var newCar = new Car
+        bool result = AddCar();
+        if (result)
         {
-            CarId = carId,
-            Make = make,
-            Model = model,
-            FuelType = fuelType,
-            Type = type,
-            Year = year,
-            Available = true
-        };
-
-        // add to data struct
-
-        // update database
-        using(var context = new CarRentalContex())
-        {
-            context.Car.Add(newCar);
-
-            context.SaveChanges();
-
+            Console.WriteLine("Car Sucessfully Added");
         }
-    }else if(choice == "2"){
+        else
+        {
+            return false;
+        }
+    }
+    else if(choice == "2"){
 
     }
     else
@@ -136,6 +107,7 @@ void AddToDatabase()
     // make same change and commit to database
 
     // check if input does not exist in db
+    return false;
 }
 
 void RemoveFromDatabase()
@@ -152,4 +124,109 @@ void RemoveFromDatabase()
 void SearchDatabase()
 {
 
+}
+
+// Add Car
+bool AddCar()
+{
+    TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+    Console.WriteLine("\n");
+    // get plate number of car
+    Console.WriteLine("Plate number:");
+    string carId = Console.ReadLine().ToUpper();
+
+    // validate car Id
+    bool idValidation = Validator.CarValidator.ValidateId(carId);
+    while (!idValidation)
+    {
+        Console.WriteLine("Car plate number invalid, enter valid Car plate number.");
+        carId = Console.ReadLine().ToUpper();
+        idValidation = Validator.CarValidator.ValidateId(carId);
+        Console.WriteLine("\n");
+    }
+
+    Console.WriteLine("\n");
+    // get make of car
+    Console.WriteLine("Make:");
+    string make = Console.ReadLine();
+
+    Console.WriteLine("\n");
+    // get model of car
+    Console.WriteLine("Model:");
+    string model = Console.ReadLine();
+
+    Console.WriteLine("\n");
+    Console.WriteLine("Fuel type:");
+    string fuelType = Console.ReadLine();
+    fuelType = textInfo.ToTitleCase(fuelType.ToLower());
+
+    Console.WriteLine("\n");
+    // validate fuel type
+    bool fuelValidation = Validator.CarValidator.ValidateFuelType(fuelType);
+    while (!fuelValidation)
+    {
+        Console.WriteLine("Fuel type invalid, enter valid fuel type");
+        fuelType = Console.ReadLine();
+        // validate fuel type
+        fuelType = textInfo.ToTitleCase(fuelType.ToLower());
+        fuelValidation = Validator.CarValidator.ValidateFuelType(fuelType);
+        Console.WriteLine("\n");
+    }
+
+    Console.WriteLine("\n");
+    Console.WriteLine("Type:");
+    string type = Console.ReadLine();
+
+    Console.WriteLine("\n");
+    Console.WriteLine("Year:");
+    //Type check
+    bool invalidYear = true;
+    int year;
+    while (invalidYear)
+    {
+        try
+        {
+            year = Int32.Parse(Console.ReadLine());
+            bool yearValidation = Validator.CarValidator.ValidateYear(year);
+            if (yearValidation)
+            {
+                invalidYear = false;
+            }
+            else
+            {
+                Console.WriteLine("Year entry invalid:Year cannot exceed current year");
+                Console.WriteLine("Please enter valid year:");
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Year entry invalid. Please enter valid year");
+        }
+        Console.WriteLine("\n");
+    }
+
+    //var newCar = new Car
+    //{
+    //    CarId = carId,
+    //    Make = make,
+    //    Model = model,
+    //    FuelType = fuelType,
+    //    Type = type,
+    //    Year = year,
+    //    Available = true
+    //};
+
+    //// add to data struct
+
+    //// update database
+    //using (var context = new CarRentalContex())
+    //{
+    //    context.Car.Add(newCar);
+
+    //    context.SaveChanges();
+
+    //}
+
+    return true;
 }
