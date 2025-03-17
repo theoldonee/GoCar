eab9c3fa9e798e2fd6a7a0303446ac7b310750e9
theoldonee <rouvaf123@gmail.com>
2025-03-17 07:57:34 +0400

    refactor: remove reduntant test
    Removed Car_ShouldBeAddedWithCorrectInputValues

--- GoCar.xUnitTest/UnitTest1.cs
+++ GoCar.xUnitTest/UnitTest1.cs
@@ -3,45 +3,6 @@ using GoCar;
 
 public class CarTests
 {
-    [Fact]
-    public void Car_ShouldBeAddedWithCorrectInputValues()
-    {
-        // Arrange (test data)
-        var carId = "Y798-AA12";  // Valid format
-        var make = "Toyota";
-        var model = "Corolla";
-        var fuelType = "Petrol";
-        var type = "Sedan";
-        var year = 2006;
-        var available = true;
-
-        // Act (validate )
-        bool isValidId = Validator.CarValidator.ValidateId(carId);
-
-        // Assert (check if validations pass)
-        Assert.True(isValidId, "CarId invalid");
-        // Create car object
-        var car = new Car
-        {
-            CarId = carId,
-            Make = make,
-            Model = model,
-            FuelType = fuelType,
-            Type = type,
-            Year = year,
-            Available = available
-        };
-
-        // Assert (make sure values were set correctly)
-        Assert.Equal(carId, car.CarId);
-        Assert.Equal(make, car.Make);
-        Assert.Equal(model, car.Model);
-        Assert.Equal(fuelType, car.FuelType);
-        Assert.Equal(type, car.Type);
-        Assert.Equal(year, car.Year);
-        Assert.True(car.Available);
-    }
-
     [Theory]
     [InlineData("Y798-AA12", true)]  // Valid carId
     [InlineData("1234-5678", false)] // Invalid CarId format
