4a5a3e200a760760ac488c71b5c6af1f2738c9bb
Melanie <bmelaniekoller@gmail.com>
2025-04-11 18:46:34 +0400

    feat: add ConsoleUI class to handle interactive menu navigation and user interaction logic

--- GoCar/ConsoleUI.cs
+++ GoCar/ConsoleUI.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 
 namespace GoCar
 {
-    
+    public class ConsoleUI
+    {
+        
+
+
+    } // end of Class:ConsoleUI
+
 }
-
-
